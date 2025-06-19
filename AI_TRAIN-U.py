828b3cd4f50d9d03233853b3357d37ac9401a1b0
Wukas7 <lucasvr777@gmail.com>
2025-06-19 22:20:47 +0200

    AI_TRAIN-U.py

--- AI_TRAIN-U.py
+++ AI_TRAIN-U.py
@@ -28,12 +28,16 @@ scopes = ["https://www.googleapis.com/auth/spreadsheets",
 
 # Carga las credenciales desde los secrets de Streamlit
 google_creds_dict = {
-  "type": st.secrets["gcp_service_account"]["type"],
-  "project_id": st.secrets["gcp_service_account"]["project_id"],
-  # ... y así con todos los campos de tu archivo credentials.json
-  "private_key": st.secrets["gcp_service_account"]["private_key"].replace('\n', '\\n'),
-  "client_email": st.secrets["gcp_service_account"]["client_email"],
-  # ... etc.
+    "type": st.secrets["gcp_service_account"]["type"],
+    "project_id": st.secrets["gcp_service_account"]["project_id"],
+    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+    "private_key": st.secrets["gcp_service_account"]["private_key"], # SIN el .replace()
+    "client_email": st.secrets["gcp_service_account"]["client_email"],
+    "client_id": st.secrets["gcp_service_account"]["client_id"],
+    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"]
 }
 
 creds = Credentials.from_service_account_info(google_creds_dict, scopes=scopes)
