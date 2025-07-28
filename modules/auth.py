185388bab16409333e3217aa2fd18fd6a973a9a7
Wukas7 <lucasvr777@gmail.com>
2025-07-28 12:28:18 +0200

    Update auth.py

--- modules/auth.py
+++ modules/auth.py
@@ -1,1 +1,16 @@
+import sqlite3
+import hashlib
 
+# Mantenemos una única conexión para toda la app
+conn = sqlite3.connect('data.db', check_same_thread=False)
+c = conn.cursor()
+
+def make_hashes(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
+def create_usertable():
+    c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT, password TEXT)')
+
+def login_user(username, password):
+    c.execute('SELECT * FROM userstable WHERE username =? AND password = ?', (username, password))
+    return c.fetchall()
