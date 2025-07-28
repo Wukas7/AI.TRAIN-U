29ba4c87414cefb277b5225396466fea2a658488
Wukas7 <lucasvr777@gmail.com>
2025-07-28 13:13:05 +0200

    Update AI_TRAIN-U.py

--- AI_TRAIN-U.py
+++ AI_TRAIN-U.py
@@ -105,9 +105,9 @@ def main():
             st.table(pd.DataFrame(plan_data).set_index("Día"))
             with st.expander("Ver Plan Original de la Semana"):
                 st.text(plan_semana_actual.get("Plan_Original_Completo", "No disponible."))
-        if st.button("👁️ Mostrar mi plan para mañana"):
 
         st.divider()
+        if st.button("👁️ Mostrar mi plan para mañana"):
 
         if "Error" in perfil_usuario:
             st.error(perfil_usuario["Error"])
